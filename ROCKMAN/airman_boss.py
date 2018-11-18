7259e5fd0a85d31959ad06e043820b5b09c1e86a
Sung Ye Sol <yessol9096@naver.com>
2018-11-18 23:01:17 +0900

    airman_boss 단계 수정

--- ROCKMAN/airman_boss.py
+++ ROCKMAN/airman_boss.py
@@ -1,36 +1,83 @@
-from pico2d import *
 import random
+import json
+import os
+
+from pico2d import *
 import game_framework
+import game_world
 
-TIME_PER_ACTION = 3   # 한번 액션하는데 0.5 걸린다
-ACTION_PER_TIME = 1.0 / TIME_PER_ACTION    # 시간당 몇번 액션 할 것인가
-FRAMES_PER_ACTION = 15
-
-class Airman:
-
-    def __init__(self, pos):
-        self.image = load_image('resource/enemy/Fan_Fined.png')
-        self.canvas_width = get_canvas_width()
-        self.canvas_height = get_canvas_height()
-        self.w = self.image.w
-        self.h = self.image.h
-        self.speed = 0
-        self.frame = random.randint(0,8)
-        self.left = 0
-        self.x = pos[0]
-        self.y = pos[1]
-        self.off_set_x = 0
-
-    def update(self):
-        self.frame = (self.frame + 8* ACTION_PER_TIME * game_framework.frame_time) % 8
-
-    def set_background(self, bg):
-        self.bg = bg
+from rockman import Rockman
+from airman_background import Airman_background
+from ariman_enemy import Tikky, Lightning_lord, Fan_fined
 
 
-    def set_center_object(self, player):
-        self.set_center_object = player
+name = "MainState"
+tikkys_position = [(1000,300), (1550, 250), (2050, 350), (2550, 330), (3050, 330), (7950, 330), (8350, 360)]
+lightning_lord_position = [(3800,350,8,8), (4100, 350, 6, 6), (4600, 350, 5, 4), (5000, 350, 5, 5)]
+fan_fineds_position = [(9600,465), (11500,465), (11750, 557)]
+player = None
 
-    def draw(self):
-        sx = self.x - (self.bg.left*3)
-        self.image.clip_draw(int(self.frame) * 40, 0, 40, 31, sx, self.y, 100, 90)
+def enter():
+    global player
+    player = Rockman()
+
+    # 적 생성, 위치조정
+    tikkys = [Tikky(tikkys_position[i]) for i in range(7)]
+    lightning_lords = [Lightning_lord(lightning_lord_position[i]) for i in range(4)]
+    fan_fineds = [Fan_fined(fan_fineds_position[i]) for i in range(3)]
+
+    background = Airman_background()
+    background.set_center_object(player)
+
+    for tikky in tikkys:
+        tikky.set_background(background)
+        tikky.set_center_object(player)
+
+    for lightning_lord in lightning_lords:
+        lightning_lord.set_background(background)
+
+    for fan_fined in fan_fineds:
+        fan_fined.set_background(background)
+    player.set_background(background)
+
+
+    game_world.add_object(background, 0)
+    game_world.add_objects(tikkys, 0)
+    game_world.add_objects(lightning_lords, 1)
+    game_world.add_objects(fan_fineds, 1)
+    game_world.add_object(player, 1)
+
+
+def exit():
+    game_world.clear()
+
+def pause():
+    pass
+
+
+def resume():
+    pass
+
+
+def handle_events():
+    events = get_events()
+    for event in events:
+        if event.type == SDL_QUIT:
+            game_framework.quit()
+        elif event.type == SDL_KEYDOWN and event.key == SDLK_ESCAPE:
+                game_framework.quit()
+        else:
+            player.handle_event(event)
+
+
+def update():
+    for game_object in game_world.all_objects():
+        game_object.update()
+    delay(0.01)
+
+
+def draw():
+    clear_canvas()
+    for game_object in game_world.all_objects():
+        game_object.draw()
+    update_canvas()
